88020fc1a626d0841969fe2f9b695d0b8114a901
Opeoluwa Lanre <opeoluwa.lanre@gmail.com>
2019-03-06 01:52:39 +0100

    Update The DB

--- v1/app.js
+++ v1/app.js
@@ -2,8 +2,20 @@ let express = require("express");
 let jwt = require("jsonwebtoken");
 let app = express();
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bucketlist', {useNewUrlParser: true});
 
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+   
+   }else {
+   let db = ('mongodb://localhost/bucketlist');
+    mongoose.connect(db, function(err){ //db = 'mongodb://localhost/yourdb'
+     if(err){
+      console.log(err);
+     }else {
+      console.log('mongoose connection is successful on: ' + db);
+     }
+    });
+   }
 let auth = require('./route/auth');
 let bucketlist = require('./route/bucketlist');
 
